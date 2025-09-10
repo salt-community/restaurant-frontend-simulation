9c4b6ac0253986be8eec4dc3311475d62d95c808
Daniel Sandstrom <github@danielsandstrom.org>
2025-09-10 17:47:54 +0200

    Disable Inventory item when available is 0

--- src/components/blocks/Order/InventoryItem.tsx
+++ src/components/blocks/Order/InventoryItem.tsx
@@ -6,9 +6,17 @@ interface InventoryItemProps {
 }
 
 export default function InventoryItem({name, description, availableAmount, onItemClick}: InventoryItemProps) {
+  const disabled = availableAmount === 0
+  let onClick = () => {}
+  if (!disabled) {
+    onClick = () => {
+      onItemClick()
+    }
+  }
   return <div
-    className="bg-gray-300 w-[500px] hover:bg-gray-400"
-    onClick={onItemClick}
+    aria-disabled={disabled}
+    className="bg-gray-300 w-[500px] aria-[disabled=false]:hover:bg-gray-400 aria-[disabled=true]:opacity-50"
+    onClick={onClick}
   >
     <a>{name}</a>
     {/*{item.name}</strong> — {item.description} (available: {item.availableAmount})*/}
